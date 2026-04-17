db583325f3181aeb1b3d075844a405a4db6eba16
garciafam777 <Richard.g.Garcianieves@gmail.com>
2026-04-16 21:43:23 -0500

    Delete Background.js

--- Background.js
+++
@@ -1,9 +0,0 @@
-// This listens for when the extension is first installed
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("MichaelAI by R&Y Productions is active.");
-});
-
-// Example: Listen for a keyboard shortcut or a specific command
-chrome.commands.onCommand.addListener((command) => {
-  console.log(`Command received: ${command}`);
-});
